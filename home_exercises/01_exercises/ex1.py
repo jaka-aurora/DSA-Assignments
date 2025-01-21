8c57ae0ebfc85e28410a62967e51241ec5c42bd9
Jaka Cosic <aa6111@student.jamk.fi>
2025-01-21 17:32:01 +0200

    add home ex 1.1

---
+++ home_exercises/01_exercises/ex1.py
@@ -0,0 +1,17 @@
+def main():
+    input_str = input("Give input:")
+    b = int(input_str)
+    kbytes = b / (2 ** 10)
+    mbytes = b / (2 ** 20)
+    gbytes = b / (2 ** 30)
+
+    print(f"bytes \t\t {b} B")
+    print(f"Kilobytes\t {(kbytes):.6f} KB")
+    print(f"Megabytes\t {(mbytes):.6f} MB")
+    print(f"Gigabytes\t {(gbytes):.6f} GB")
+
+    if __name__ == '__main__':
+        main()
+
+# model 01-02
+
